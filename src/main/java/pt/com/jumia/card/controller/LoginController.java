2c8f5e59845403011daa16ed835f6724eb060139
Augusto Colombelli Alessio <augustocolombelli@gmail.com>
2018-12-12 18:59:01 -0200

    remove

--- src/main/java/pt/com/jumia/card/controller/LoginController.java
+++ src/main/java/pt/com/jumia/card/controller/LoginController.java
@@ -10,7 +10,6 @@ public class LoginController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String showLoginPage(ModelMap model) {
-		model.put("name", "in28Minutes");
 		return "welcome";
 	}
 
